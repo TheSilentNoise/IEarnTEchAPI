2029e0a5a98bca974ce72d156c708b8362ffe977
Ubuntu <ubuntu@ip-172-31-13-240.us-west-1.compute.internal>
2018-03-27 16:09:49 +0000

    Thread added

--- src/apps.py
+++ src/apps.py
@@ -19,4 +19,4 @@ api.add_resource(UserDetails,'/IEarn-Tech/api/v1/users')
 api.add_resource(ChangePassword,'/IEarn-Tech/api/v1/users/<int:userId>/changePassword')
 
 if __name__ == '__main__':
-    app.run(debug=True, host= '0.0.0.0', port=8000)
+    app.run(debug=True, host= '0.0.0.0', port=8000, threaded=True)
